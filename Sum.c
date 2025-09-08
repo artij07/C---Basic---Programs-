7749c900340d4a6da648a8570b3e15ad5c68c8f9
Arti Jadhav <artij07@gmail.com>
2025-09-08 19:56:16 +0530

    Added Sum program

---
+++ Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 100000;   
+    int b = 25000;  
+    int sum;
+    sum = a + b;
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+     return 0;
+}
